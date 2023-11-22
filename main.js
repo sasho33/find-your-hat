122ed7805ed82bc2890bce0aa4d71a675f0e196e
sasho33 <sasho3333333@gmail.com>
2023-11-22 22:22:03 +0000

    class created with printing field

--- main.js
+++ main.js
@@ -1,6 +1,29 @@
-const prompt = require('prompt-sync')({sigint: true});
+const prompt = require('prompt-sync')({ sigint: true });
 
 const hat = '^';
 const hole = 'O';
 const fieldCharacter = '░';
 const pathCharacter = '*';
+
+class Field {
+  constructor(fieldArray) {
+    this.field = fieldArray;
+    this.fieldRow = 0;
+    this.fieldCol = 0;
+    this.gameOver = false;
+  }
+
+  print() {
+    for (let row = 0; row < this.field.length; row++) {
+      console.log(this.field[row].join(' '));
+    }
+  }
+}
+
+const myField = new Field([
+  ['*', '░', 'O'],
+  ['░', 'O', '░'],
+  ['░', '^', '░'],
+]);
+
+myField.print();
